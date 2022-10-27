147f3d8f24edfd72c5974fc4b05475b99213b210
daniil19va <daniil19va@gmail.com>
2022-10-27 19:46:10 +0300

    Lesson2: add new big comment

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world" <<endl;
+    cout << "Hello from linux" <<endl;
+
+    return 0;
+}
+/***
+
+big new comment
+
+***/
