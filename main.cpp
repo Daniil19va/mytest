e0773ae8a5942111be897f83aeaf07635ff5704a
daniil19va <daniil19va@gmail.com>
2022-10-27 19:34:28 +0300

    Initial 3

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a,int b)
+{
+    return a + b;
+}
+
+
+int main()
+{
+    cout << "Hello world" << endl;
+    cout << "Hello from linux" << endl;
+    cout << "2 + 3 = " << add(2,3) <<endl;
+    cout << "3 + 8 = " << add(3,8) <<endl;
+    return 0;
+}
+
